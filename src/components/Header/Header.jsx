c123218210e4d1573aa39bbe4cfcabbc9750e7a1
Laura Nguyen <hello@lauranguyen.me>
2024-06-06 16:30:54 -0400

    styled header to make it responsive for all devices

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import "./Header.scss";
+import logo from '../../assets/images/BrainFlix-logo.svg';
 
 function Header() {
     return (
         <header className="header">
-            <img className="header__logo" src="src/assets/images/BrainFlix-logo.svg" alt="BrainFlix Logo" />
+            <img className="header__logo" src={logo} alt="BrainFlix Logo" />
             <div className="header__content">
-                    <input className="header__search-bar" type="text" placeholder="Search"/>
-                    <img className="header__profile-pic" src="src/assets/images/Mohan-muruge.jpg" alt="side profile of Mohan" />
-             
+                <div className="header__search-profile">
+                    <input className="header__search-bar" type="text" placeholder="Search" />
+                    <img className="header__profile-pic header__profile-pic--mobile" src="src/assets/images/Mohan-muruge.jpg" alt="side profile of Mohan" />
+                </div>
                 <button className="header__button--upload" type="submit">Upload</button>
+                <img className="header__profile-pic header__profile-pic--desktop" src="src/assets/images/Mohan-muruge.jpg" alt="side profile of Mohan" />
             </div>
         </header>
     )
