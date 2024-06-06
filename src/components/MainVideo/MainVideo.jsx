405c88a358046e3c208e8a438145d042c57aa2bb
Laura Nguyen <hello@lauranguyen.me>
2024-06-06 01:23:29 -0400

    finished initial structure of info for main video

--- src/components/MainVideo/MainVideo.jsx
+++ src/components/MainVideo/MainVideo.jsx
@@ -1,6 +1,9 @@
 import "./MainVideo.scss";
 
 const MainVideo = ({title, channel, image, description, views, likes, duration, video, timestamp}) => {
+    
+    const formattedDate = new Date(timestamp).toLocaleString("en-US", { year: "numeric", month: "2-digit", day: "2-digit" })
+    
     return (
         <main className="main-video">
         
@@ -10,15 +13,19 @@ const MainVideo = ({title, channel, image, description, views, likes, duration, 
         <div className="main-video__content">
             <h1 className="main-video__title">{title}</h1>
             <div className="main-video__info">
-                <p className="main-video__author">{channel}</p>
-                <p className="main-video__date">{timestamp}</p>
-                <div className="main-video__views">
-                    <img src="src/assets/icons/views.svg" alt="views icon" />
-                    <p className="main-video__views-count">{views}</p>
+                <div className="main-video__upload">
+                    <p className="main-video__author">{`By ${channel}`}</p>
+                    <p className="main-video__date">{formattedDate}</p>
                 </div>
-                <div className="main-video__likes">
-                    <img src="src/assets/icons/likes.svg" alt="likes icon" />
-                    <p className="main-video__likes-count">{likes}</p>
+                <div className="main-video__stats">
+                    <div className="main-video__views">
+                        <img src="src/assets/icons/views.svg" alt="views icon" />
+                        <p className="main-video__views-count">{views}</p>
+                    </div>
+                    <div className="main-video__likes">
+                        <img src="src/assets/icons/likes.svg" alt="likes icon" />
+                        <p className="main-video__likes-count">{likes}</p>
+                    </div>
                 </div>
             </div>
             <p className="main-video__description">{description}</p>
