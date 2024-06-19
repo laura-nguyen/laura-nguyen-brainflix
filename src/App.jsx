84cf314ee7e7e1d0b5976c4a33424ccd5e996dee
Laura Nguyen <hello@lauranguyen.me>
2024-06-19 00:02:11 -0400

    final commit for sprint-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import './App.scss';
 
-import videos from './data/video-details.json';
 import Header from './components/Header/Header';
-import MainVideoPlayer from './components/MainVideoPlayer/MainVideoPlayer';
-import MainVideoDetails from './components/MainVideoDetails/MainVideoDetails';
-import CommentsSection from './components/CommentsSection/CommentsSection';
-import VideoList from './components/VideoList/VideoList';
-
 import VideoPage from "./pages/VideoPage/VideoPage";
 import UploadPage from "./pages/UploadPage/UploadPage";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
@@ -28,37 +21,6 @@ function App() {
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
- 
-      {/* <Header />  
-
-      <MainVideoPlayer
-        image={mainVideo.image}
-        video={mainVideo.video}
-        timestamp={mainVideo.timestamp}
-      />
-      
-      <div className="app--desktop">
-        <div className="app--tablet">
-          <MainVideoDetails 
-          title={mainVideo.title}
-          channel={mainVideo.channel}
-          description={mainVideo.description}
-          views={mainVideo.views}
-          likes={mainVideo.likes}
-          timestamp={mainVideo.timestamp}
-          />
-
-          <CommentsSection
-            comments={mainVideo.comments}
-          />
-        </div>
-    
-        <VideoList
-          videos={filteredVideos}
-          changeMainVideo={changeMainVideo}
-        />
-
-        </div> */}
 
     </>
   )
