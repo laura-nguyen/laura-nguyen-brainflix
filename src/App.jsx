a5b4ffa6d8660ed0de62c2fa56550cbd684094c0
Laura Nguyen <hello@lauranguyen.me>
2024-06-20 22:54:22 -0400

    switched to express api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import './App.scss';
 
-import videos from './data/video-details.json';
 import Header from './components/Header/Header';
-import MainVideoPlayer from './components/MainVideoPlayer/MainVideoPlayer';
-import MainVideoDetails from './components/MainVideoDetails/MainVideoDetails';
-import CommentsSection from './components/CommentsSection/CommentsSection';
-import VideoList from './components/VideoList/VideoList';
 
 import VideoPage from "./pages/VideoPage/VideoPage";
 import UploadPage from "./pages/UploadPage/UploadPage";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
+
+  // const baseURL = import.meta.env.VITE_API_URL; 
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const { data } = await axios.get(`${baseURL}/videos`);
+  //   }
+  //   fetchData();
+  // }, [])
 
   return (
     <>
@@ -28,37 +32,6 @@ function App() {
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
- 
-      {/* <Header />  
-
-      <MainVideoPlayer
-        image={mainVideo.image}
-        video={mainVideo.video}
-        timestamp={mainVideo.timestamp}
-      />
-      
-      <div className="app--desktop">
-        <div className="app--tablet">
-          <MainVideoDetails 
-          title={mainVideo.title}
-          channel={mainVideo.channel}
-          description={mainVideo.description}
-          views={mainVideo.views}
-          likes={mainVideo.likes}
-          timestamp={mainVideo.timestamp}
-          />
-
-          <CommentsSection
-            comments={mainVideo.comments}
-          />
-        </div>
-    
-        <VideoList
-          videos={filteredVideos}
-          changeMainVideo={changeMainVideo}
-        />
-
-        </div> */}
 
     </>
   )
